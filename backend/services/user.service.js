640c020d5886baa665e5a34865aa72fef5ba46d8
Mradul Jain <mraduljain184@gmail.com>
2025-08-16 20:44:25 +0530

    final authentication done

--- backend/services/user.service.js
+++ backend/services/user.service.js
@@ -1,21 +1,20 @@
-const userModel = require("../models/user.model");
+const userModel = require('../models/user.model');
+
 
 module.exports.createUser = async ({
-    firstname,
-    lastname,
-    email,
-    password
-})=>{
-    if(!firstname || !email || !password) {
-        throw new Error("All fields are required");
+    firstname, lastname, email, password
+}) => {
+    if (!firstname || !email || !password) {
+        throw new Error('All fields are required');
     }
     const user = userModel.create({
         fullname: {
             firstname,
             lastname
         },
-        email, 
+        email,
         password
-    });
+    })
+
     return user;
 }
