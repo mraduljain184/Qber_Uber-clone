847e89adbec8dda5e42a8ce8d1b616341b29f1d0
Mradul Jain <mraduljain184@gmail.com>
2025-08-24 20:55:09 +0530

    Frontend ready

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+import UserContext from './context/UserContext.jsx';
+import CaptainContext from './context/CaptainContext.jsx';
+import SocketProvider from './context/SocketContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </StrictMode>,
+
+  <CaptainContext>
+    <UserContext>
+      <SocketProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SocketProvider>
+    </UserContext>
+  </CaptainContext>
+
 )
