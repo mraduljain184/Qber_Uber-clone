7c92b91bcbb1d9c8646d4b2e45c91c3f0accefd8
mraduljain184 <mraduljain184@gmail.com>
2025-09-16 01:02:17 +0530

    Backend edits

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,60 @@
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
+// dotenv.config();
+// const cors = require("cors");
+// const express = require("express");
+// const app = express();
+// const connectToDB = require("./db/db");
+// connectToDB();
+// const userRoutes = require("./routes/user.routes");
+// const cookieParser = require("cookie-parser");
+// const captainRoutes = require("./routes/captain.routes");
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+
+// app.get("/", (req, res) => {
+//   res.send("Hello, World!");
+// });
+
+// app.use('/users', userRoutes);
+// app.use('/captains', captainRoutes);
+
+// module.exports = app;
+
+
+const dotenv = require('dotenv');
 dotenv.config();
-const cors = require("cors");
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const connectToDB = require("./db/db");
-connectToDB();
-const userRoutes = require("./routes/user.routes");
-const cookieParser = require("cookie-parser");
-const captainRoutes = require("./routes/captain.routes");
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
+
+connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
+
+
+
 
 module.exports = app;
